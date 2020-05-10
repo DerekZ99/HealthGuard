b6e75113b7feb588d366ca5dfba1c06a5a1d8ff3
Derek <346230944@qq.com>
2020-05-11 00:51:49 +0800

    css work

--- healthGuard/healthguard/src/pages/writeJobPost/index.vue
+++ healthGuard/healthguard/src/pages/writeJobPost/index.vue
@@ -5,12 +5,36 @@
     <form @submit="formSubmit" class="job-form">
       <view class="form-item">
         <view class="item-title">姓名：</view>
-        <input type="text" placeholder="请输入您的姓名" name="posterName" />
+        <view class="input-wrap">
+          <input
+            type="text"
+            placeholder="请输入您的姓名"
+            name="posterName"
+            v-model="textValue1"
+            @blur="checkVal(textValue1,'posterName')"
+            :class="{warning:!isNameOk}"
+          />
+          <!-- 警告信息 -->
+          <view class="warning-text" v-show="!isNameOk">请不要输入空格或符号</view>
+          <!-- 警告信息 -->
+        </view>
       </view>
 
       <view class="form-item">
         <view class="item-title">联系方式：</view>
-        <input type="text" placeholder="请输入您的手机号码" name="phone" />
+        <view class="input-wrap">
+          <input
+            type="number"
+            placeholder="请输入您的手机号码"
+            name="phone"
+            v-model="textValue2"
+            @blur="checkVal(textValue2,'phone')"
+            :class="{warning:!isPhoneOk}"
+          />
+          <!-- 警告信息 -->
+          <view class="warning-text" v-show="!isPhoneOk">请输入正确的手机号码</view>
+          <!-- 警告信息 -->
+        </view>
       </view>
 
       <view class="form-item">
@@ -23,17 +47,15 @@
         <textarea minlength="-1" type="text" placeholder="请输入您可提供的服务" name="service" />
       </view>
 
-      <view class="img">
-        <view class="item-title">
-          个人照片：
-          <span class="imgInfo">请上传真实的个人照片</span>
-          <view class="imgBox">
-            <view class="imgShow">
-              <img mode="widthFix" :src="imgFilePath" alt />
-            </view>
-            <button @click="handleChooseImg" class="chooseImg">选择图片</button>
-          </view>
+      <view class="form-item">
+        <view class="item-title">个人照片：</view>
+      </view>
+      <view class="imgBox">
+        <span class="imgInfo">请上传真实的个人照片</span>
+        <view class="imgShow">
+          <img mode="widthFix" :src="imgFilePath" alt />
         </view>
+        <button @click="handleChooseImg" class="chooseImg">选择图片</button>
       </view>
 
       <view class="btns">
@@ -50,7 +72,9 @@ export default {
     return {
       formTitle: "",
       imgFilePath:
-        "cloud://tryout-edov9.7472-tryout-edov9-1302058975/imgHolder.jpg"
+        "cloud://tryout-edov9.7472-tryout-edov9-1302058975/imgHolder.jpg",
+      isNameOk: true,
+      isPhoneOk: true
     };
   },
   onLoad(options) {
@@ -62,10 +86,12 @@ export default {
       let that = this;
       // console.log(e);
       wx.cloud.uploadFile({
-        cloudPath: "posterImg/text",
+        cloudPath: `posterImg/${new Date().getTime()}-${Math.floor(
+          Math.random() * 1000
+        )}`,
         filePath: that.imgFilePath,
         success(res) {
-          console.log("图片被上传到了云存储中");
+          res.fileID;
         },
         fail(err) {
           console.log(err + "上传失败");
@@ -82,6 +108,18 @@ export default {
           that.imgFilePath = res.tempFilePaths[0];
         }
       });
+    },
+    // 正则验证
+    checkVal(val, name) {
+      let regPhone = /((\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$)/;
+
+      let regName = /^[\u4E00-\u9FA5A-Za-z]+$/;
+
+      if (name === "posterName") {
+        this.isNameOk = regName.test(val);
+      } else {
+        this.isPhoneOk = regPhone.test(val);
+      }
     }
   }
 };
@@ -106,33 +144,36 @@ export default {
 
     .item-title {
       font-weight: 500;
+      flex: 0.3;
+      display: flex;
+      justify-content: flex-end;
     }
+
     textarea {
+      flex: 0.7;
       height: 140rpx;
     }
   }
-  .img {
-    padding: 20rpx;
-    border-top: 3rpx solid #ccc;
-    font-size: 30rpx;
 
-    .imgBox {
-      .imgShow {
-        margin: 20rpx auto;
-        height: 240rpx;
-        width: 240rpx;
-        overflow: hidden;
-        border: 3rpx solid #cccccc;
+  .imgBox {
+    .imgInfo {
+      display: flex;
+      justify-content: center;
+    }
+    .imgShow {
+      margin: 20rpx auto;
+      height: 200rpx;
+      width: 200rpx;
+      overflow: hidden;
+      border: 3rpx solid #cccccc;
 
-        img {
-          width: 100%;
-        }
+      img {
+        width: 100%;
       }
-
-      .chooseImg {
-        width: 60vw;
-        margin: 0 auto;
-      }
+    }
+    .chooseImg {
+      width: 60vw;
+      margin: 0 auto;
     }
   }
 
@@ -147,5 +188,19 @@ export default {
       margin: 0 auto;
     }
   }
+}
+.input-wrap {
+  flex: 0.7;
+  display: flex;
+  flex-direction: column;
+}
+.warning-text {
+  margin: 10rpx;
+  color: red;
+  font-size: 30rpx;
+  justify-self: start;
+}
+.warning {
+  border: 3rpx solid red;
 }
 </style>
